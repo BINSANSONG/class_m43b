f06a8cbb6c7b703c28050a29a9d13b3a4d6210cc
유태영 <neovansoarer@gmail.com>
2018-11-05 14:49:42 +0900

    Set giphy API

--- VanillaJS/giphy-search-engine/main.js
+++ VanillaJS/giphy-search-engine/main.js
@@ -1,5 +1,4 @@
 // 1. <input> 태그 안의 값을 잡는다.
-
 const button = document.querySelector('#js-button');
 const inputArea = document.querySelector('#js-input');
 
@@ -8,17 +7,19 @@ button.addEventListener('click', () => {
   pushToDOM(inputValue);
 });
 
-inputArea.addEventListener('keyup', (e) => {
+inputArea.addEventListener('keypress', (e) => {
   const inputValue = document.querySelector('#js-input').value;
   if(e.which === 13) pushToDOM(inputValue);
 });
 
-
-
 // 2. API 를 활용해 data 를 받는다. 그리고 가공한다.
+const API_KEY = 'pETal56SsBpImEECWh6AcSOUlSZfg7zU';
+let keyword = 'cats';
+const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`
+console.log(URL);
 
 // 3. GIF 파일들을 index.html 에 밀어 넣는다.
-const pushToDOM = (data) => {
+const pushToDOM = data => {
   const resultArea = document.querySelector('#result-area');
   resultArea.innerHTML = data;
 };
