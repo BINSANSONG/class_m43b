56c44c3de72aad1de326f234be3240749c68c32f
유태영 <neovansoarer@gmail.com>
2018-11-05 13:49:34 +0900

    ~2pm

---
+++ VanillaJS/giphy-search-engine/main.js
@@ -0,0 +1,6 @@
+// 1. <input> 태그 안의 값을 잡는다.
+const inputValue = document.querySelector('#js-input').value;
+console.log(inputValue);
+// 2. API 를 활용해 data 를 받는다. 그리고 가공한다.
+
+// 3. GIF 파일들을 index.html 에 밀어 넣는다.
