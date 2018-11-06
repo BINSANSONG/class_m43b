bb18542d0e96a9c4e1faa56e9c1cabe791a79fa2
유태영 <neovansoarer@gmail.com>
2018-11-06 10:02:36 +0900

    Ready to build sound cloud player

--- VanillaJS/sound-cloud-player/javascript/main.js
+++ VanillaJS/sound-cloud-player/javascript/main.js
@@ -1,7 +1,25 @@
 /* 1. 검색 */
 
 /* 2. SoundCloud API  사용하기 */
-client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+const SoundCloudAPI = {
+  init: () => {
+    SC.initialize({
+      client_id: "cd9be64eeb32d1741c17cb39e41d254d"
+    });
+  },
+
+  getTrack: (inputValue) => {
+    SC.get("/tracks", {
+      q: inputValue
+    }).then(function(tracks) {
+      console.log(tracks);
+    });
+  }
+};
+
+SoundCloudAPI.init();
+SoundCloudAPI.getTrack('coffee');
+
 /* 3. 카드 보여주기 */
 
 /* 4. Playlist 에 추가하고 실제로 재생하기 */
