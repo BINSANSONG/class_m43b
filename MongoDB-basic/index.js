fc4d2f531ec2ffa32cfee78c3cd153550b7e6505
유태영 <neovansoarer@gmail.com>
2018-11-14 11:17:03 +0900

    Finish Basic and Validation

--- MongoDB-basic/index.js
+++ MongoDB-basic/index.js
@@ -5,11 +5,25 @@ mongoose.connect('mongodb://localhost/hello-mongo', { useNewUrlParser: true })
   .catch(error => console.error(error.message));
 
 // Available schema Datatypes 
-// : String, Number, Date, Buffer, Boolean, ObjectID, Array
+  // String, Number, Date, Buffer, Boolean, ObjectID, Array
+// Available Validatinog options
+  // String: minlength, maxlength, match, enum
+  // Numbers, Dates: min, max
+  // All: reauired
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true , minlength: 2 },
   author: String,
-  tags: [ String ],
+  tags: {
+    type: Array,
+    // custom Valdator
+    validate: {
+      validator: function(tags) { 
+        const result = tags.every(tag => tag.length > 0);
+        return tags && tags.length > 0 && result;
+      },
+      message: 'A Course should have at least 1 tag'
+    }
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean
 });
@@ -17,57 +31,68 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema);
 
 /* CRUD Operation */
-
+  /* Create */
 async function createCourse() {
   const course = new Course({
-    name: '실전 DApp 빌드',
-    author: 'john',
-    tags: ['Ethereum', 'Blockchain', 'DApp'],
-    isPublished: false
+    name: 'aa',
+    author: 'js',
+    tags: ['a', 'b'],
+    isPublished: true
   });
-  try{
+
+  try {
     const result = await course.save();
     console.log(result);
   } catch(error) {
-    console.error(error.mesage);
+    console.error(error.message);
   }
 }
 
+createCourse();
+
+/* Retrieve */
 async function getCourses() {
   const courses = await Course
-    //.find({ price: { $lt: 15, $gt: 10 })
-    //.find({ price: { $in: [10, 15] } })
-    // .find({ isPublished: true })
-    // .limit(10)
-    // .sort({ name: -1 })
-    // .select({ name: 1, tags: 1 })
-    // .select('name tags')
-    // .find({ author: /^ne/i })
-    // .find({ author: /hn$/})
-    // .find({ author: /.*oh.*/})
-    // .count()
+    .find()
 
   console.log(courses);
 }
-/* 비교 쿼리 연산자 
-  $eq (equal)
-  $neq (not equal)
-  $gt (greater than)
-  $gte (greater than or equal to)
-  $lt (less than)
-  $lte
-  $in
-  $nin
-*/
+// getCourses();
 
-/* 논리 쿼리 연산자
-  .or
-    Course.find()
-      .or([{ author: 'neo' }, { isPublished: false }])
-  .and
-    Course.find()
-      .and([{ author: 'neo' }, { isPublished: false }])
-*/
+/* Update */
+// 1. Qurey First: find => change => save
+async function updateCourse(id) {
+  // Find
+  const course = await Course.findById(id)
+  // const course = await Course.find({ _id: '3424432432dsafdsds' })
+  if(!course) return;
 
+  // Change
+  course.author = 'hphk-YEAH';
+  course.tags = ['SDFSFD'];
+  
+  // Save
+  const result = await course.save();
+  console.log(result);
+}
 
-getCourses();
+// updateCourse("5bea693dcc15484591576596");
+
+// 2. Update First :직접 Update => result
+async function updateCourses(id) {
+  const result = await Course.updateMany({ isPublished: true }, {
+    $set
+  })
+}
+
+// updateCourses('5bea693dcc15484591576596');
+
+/* Destroy */
+async function removeCourse(id) {
+  const result = await Course.deleteOne({ _id: id });
+  // const course = await Course.findOneAndDelete();
+  console.log(result);
+
+}
+
+// removeCourse('5bea693dcc15484591576596');
